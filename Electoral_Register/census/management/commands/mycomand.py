391575e70432217a1a9de1aff05d48234f2c747d
Lorias95 <luis.orias.aguilar@est.una.ac.cr>
2021-08-12 00:45:50 -0600

    update mycommand

--- Electoral_Register/census/management/commands/mycomand.py
+++ Electoral_Register/census/management/commands/mycomand.py
@@ -1,5 +1,9 @@
 import argparse
 import threading
+import logging
+import time
+logger = logging.getLogger(__name__)
+from django.db import connection
 from django.core.management.base import BaseCommand
 
 def procesar_archivos(options, numthread, contador, max_contador, resultado):
@@ -19,20 +23,51 @@ def procesar_archivos(options, numthread, contador, max_contador, resultado):
 
 
 class Command(BaseCommand):
-    help = "this is a command import tse padron developed in solvo training by Luis Diego Orias A."
+    help = "this is a command import TSE Padron developed in solvo training by Luis Diego Orias A."
 
-    #Comando para procesar el archivo PADRON.txt o MiPadron.txt
+    #Comando para procesar el archivo PADRON_COMPLETO.txt o MiPadron.txt
+
+    #Ejecución del comando en windows:
 
     #python manage.py mycomand "C:\\Users\\USUARIO\\PycharmProjects\\ElectoralRegisterProject\\MyData\\PADRON_COMPLETO.txt"
 
-    #Muestra del Padron Utilizada en total 200 registros para el ejercicio.
+    #La muestra extraida del padron utilizada fueron 898 lineas y se usarán en total 200 registros para el ejercicio.
 
     #python manage.py mycomand "C:\\Users\\Xinia Aguilar\\PycharmProjects\\ElectoralRegisterProject\\MyData\\MiPadron.txt" -t 200
+
+    def clean_tables(self):
+        """
+        Delete all data on database if exists.
+        """
+        print("Deleting all registry data")
+        with connection.cursor() as cursor:
+            logger.debug("Execute 'TRUNCATE `padronelectoral_elector`' ")
+            # Delete in raw for optimization
+            cursor.execute('TRUNCATE `padronelectoral_elector`')
+
+        # Using cascade aproach to delete other tables
+        print('province'.objects.all().delete())
+
+    def calculate_speed(self, func, message, *args):
+        """
+        Calculate the time spend by a function call
+
+        :param func:  Function to call
+        :param message: Message to print when time speed is available
+        :param args:  Arguments to pass to functions
+        """
+        start = time.time()
+        func(*args)
+        end = time.time()
+        print("Complete %s in " % message, end - start, " s")
+
 
     def add_arguments(self, parser):
         parser.add_argument("diselect", type=argparse.FileType('r', encoding='latin-1'))
         #parser.add_argument("padron", type=argparse.FileType('r', encoding='latin-1'))
         parser.add_argument("-t","--threads", type=int , dest='threads', default=1)
+
+        parser.add_argument('--truncate',dest='truncate',default=200, type=int, help='Max number of elements inserted by statement', )
 
     def handle(self, *args, **options):
         #print(args, options)
